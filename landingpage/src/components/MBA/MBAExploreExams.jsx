36f1be1868434783ef0b27e5da078c5c5912afb0
Jyothirmai08 <jyothirmai.k@egradtutor.in>
2023-10-04 17:29:11 +0530

    mba page commit

--- landingpage/src/components/MBA/MBAExploreExams.jsx
+++ landingpage/src/components/MBA/MBAExploreExams.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { MBAExploreExamData } from './MBAExploreExamsData'
-import "../UG/Main.css"
 import '../UG/ExploreExams.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import img1 from './images/BITSAT.png'
 
 
 export const MBAExploreExams = () => {
@@ -12,33 +12,39 @@ export const MBAExploreExams = () => {
         <div className='explore-container container' id='Exams'>
             <div className='explore-contantText'><h1>EXPLORE EXAMS</h1></div>
 
-            <div className='cards-container'>
+
+            <div className='mbaexampage'>
+                <div className='exmapageimg'>
+                    <img src={img1} alt="" />
+                </div>
+
+                <div className='cards-container'>
                 {MBAExploreExamData.map((item,index) => {
                     return(
                         <div key={index} className={item.cardContant}>
                           
                                 <div className='cardFlex' >
-                                    {/* This is the image */}
                                   <div className='hand-div'>
                                  
                                    <Link to={item.examPage}><h4>{item.Title}</h4></Link>
                                   <div className='hand' ><i class="fa-solid fa-hand-point-left"></i></div>
 
                                   </div>
-
-                                    {/* <div className='cardImg'><img width={180} height={150} src={item.IMGuRL}alt='' /></div> */}
-                                {/* This is the content */}
                                     <div className='CoursesTextIcons'>
-                                       <Link to={item.explore}><h5>{item.OnlineTestSeries} <i class="fa-solid fa-file-lines"></i></h5></Link>
-                                       <Link to={item.liveclasses}><h5>{item.OLVClasses}<i class="fa-solid fa-tv"></i></h5></Link>
+                                       <Link to={item.explore}><p>{item.OnlineTestSeries} <i class="fa-solid fa-file-lines"></i></p></Link>
+                                       <Link to={item.liveclasses}><p>{item.OLVClasses}<i class="fa-solid fa-tv"></i></p></Link>
                                         
-                                        <h5>{item.ONLINE}</h5>
                                     </div>
                                 </div>
                         </div>
                     )
                 })}
             </div>
+
+
+            </div>
+
+            
         </div>
     </div>
   )
